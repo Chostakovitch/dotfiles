73a0ed35bf99f5d22271ec95431ee6ead4448fe6
Quentin Duchemin <quentinduchemin@gmail.com>
2018-05-16 00:29:14 +0200

    Improved i3, polybar and terminator config
    Support for lock screen, suspend-to-RAM, i3-gaps. Better handle killing polybar. Support for volume and media keys. Autolock also triggers i3lock. Better handling popups.

--- .config/polybar/scripts/launch_polybar.sh
+++ .config/polybar/scripts/launch_polybar.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 names=("chostybottombar" "chostytopbar")
-pkill polybar
+killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if type "xrandr"; then
   for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     for name in ${names[@]}; do
-      MONITOR=${monitor} polybar -q --reload ${name}&
+      MONITOR=${monitor} polybar -q --reload ${name} &
     done
   done
 else
