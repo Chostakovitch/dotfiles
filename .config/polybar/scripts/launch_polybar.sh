ad3005b3eaf3df8f50cb9b984a1970019b3cfe14
Quentin Duchemin <quentinduchemin@gmail.com>
2018-06-09 12:29:19 +0200

    Show polybar only under on screen

--- .config/polybar/scripts/launch_polybar.sh
+++ .config/polybar/scripts/launch_polybar.sh
@@ -4,10 +4,10 @@ names=("chostybottombar" "chostytopbar")
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 if type "xrandr"; then
-  for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    for name in ${names[@]}; do
-      MONITOR=${monitor} polybar -q --reload ${name} &
-    done
+  monitors=$(xrandr --query | grep " connected" | cut -d" " -f1)
+  monarray=($monitors)
+	for name in ${names[@]}; do
+	  MONITOR=${monarray[1]} polybar -q --reload ${name} &
   done
 else
   polybar --reload ${name} &
