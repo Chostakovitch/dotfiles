776a1e558866cb82936e75549b735287adbb175d
Quentin Duchemin <quentinduchemin@gmail.com>
2019-01-10 10:13:42 +0100

    Removed references to GMail, enhance usb mount

--- .config/polybar/scripts/system-usb-udev.sh
+++ .config/polybar/scripts/system-usb-udev.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
+# Dependencies : jq ; udisks2
 # Some updates by Quentin Duchemin on 18/03/07
 # - Add label in lsblk selection
 # - Add symbols from Material Google font
 # - Remove term emulator opening and unused parts
 # - Remove udiskctl power-off because we cannot mount again the removable device
 # - Change printed name from vendor of the device to partition name (arbitrary choice)
+# On 18/11/20
+# - Change jq rm == '1' to rm == true
 usb_print() {
     devices=$(lsblk -Jplno NAME,TYPE,RM,SIZE,MOUNTPOINT,VENDOR,LABEL)
     output=""
     counter=0
 
-    for unmounted in $(echo "$devices" | jq -r '.blockdevices[]  | select(.type == "part") | select(.rm == "1") | select(.mountpoint == null) | .name'); do
+    for unmounted in $(echo "$devices" | jq -r '.blockdevices[]  | select(.type == 
+"part") | select(.rm == true) | select(.mountpoint == null) | .name'); do
         unmounted=$(echo "$devices" | jq -r '.blockdevices[]  | select(.name == "'"$unmounted"'") | .label')
         unmounted=$(echo "$unmounted" | tr -d ' ')
 
@@ -25,7 +29,8 @@ usb_print() {
         output="$output$space $unmounted"
     done
 
-    for mounted in $(echo "$devices" | jq -r '.blockdevices[] | select(.type == "part") | select(.rm == "1") | select(.mountpoint != null) | .size'); do
+    for mounted in $(echo "$devices" | jq -r '.blockdevices[] | select(.type == 
+"part") | select(.rm == true) | select(.mountpoint != null) | .size'); do
         if [ $counter -eq 0 ]; then
             space=""
         else
@@ -54,7 +59,8 @@ case "$1" in
     --mount)
         devices=$(lsblk -Jplno NAME,TYPE,RM,MOUNTPOINT)
 
-        for mount in $(echo "$devices" | jq -r '.blockdevices[]  | select(.type == "part") | select(.rm == "1") | select(.mountpoint == null) | .name'); do
+        for mount in $(echo "$devices" | jq -r '.blockdevices[]  | select(.type == 
+"part") | select(.rm == true) | select(.mountpoint == null) | .name'); do
             mountpoint=$(udisksctl mount --no-user-interaction -b "$mount")
         done
 
@@ -63,7 +69,8 @@ case "$1" in
     --unmount)
         devices=$(lsblk -Jplno NAME,TYPE,RM,MOUNTPOINT)
 
-        for unmount in $(echo "$devices" | jq -r '.blockdevices[]  | select(.type == "part") | select(.rm == "1") | select(.mountpoint != null) | .name'); do
+        for unmount in $(echo "$devices" | jq -r '.blockdevices[]  | select(.type == 
+"part") | select(.rm == true) | select(.mountpoint != null) | .name'); do
             udisksctl unmount --no-user-interaction -b "$unmount"
             #power-off makes us unable to mount again...
             #udisksctl power-off --no-user-interaction -b "$unmount"
